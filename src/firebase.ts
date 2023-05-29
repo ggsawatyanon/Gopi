a948f2a56285de46357e2707967438e3c0ddbe44
kevin <ktb53@cornell.edu>
2023-05-29 12:22:06 +0800

    Created React Project and connected to Firebase with Hosting and Firestore enabled

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7GtNcTwNgfC7ykSGvzQdMSpkAqZRb3aE",
+  authDomain: "gopi-cu.firebaseapp.com",
+  projectId: "gopi-cu",
+  storageBucket: "gopi-cu.appspot.com",
+  messagingSenderId: "527293978389",
+  appId: "1:527293978389:web:a46a0c5ccda68e1856fb1c",
+  measurementId: "G-T10KEE6BRK"
+};
+
+firebase.initializeApp(firebaseConfig)
