dba75bd40656eda7246eaabec058f9da57dd599f
TushigBili <tushig2003@gmail.com>
2023-07-18 21:49:54 +0800

    Kevin Commit add page back end (with plus button)

--- frontend/src/components/Add-New-Set.tsx
+++ frontend/src/components/Add-New-Set.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardContent, Grid, makeStyles, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { TextField, CardContent, Grid, makeStyles, Typography, Button } from '@material-ui/core';
 import { colors } from '../colors';
 import { TbEditCircle } from 'react-icons/tb'
 import { FaPlayCircle } from 'react-icons/fa'
-// import { ReactComponent as PlusIcon } from '../assets/plus-solid.svg';
-// import PlusIcon from '../../assets/plus-solid.svg';
+import { doc, addDoc, collection } from "firebase/firestore";
+import { db } from '../firebase'
 
 const useStyles = makeStyles({
     container: {
@@ -61,7 +60,13 @@ const useStyles = makeStyles({
         marginRight: '7.5vw',
         justifyContent: 'space-evenly',
         borderTopLeftRadius: 20,
-        borderTopRightRadius: 20
+        borderTopRightRadius: 20,
+        overflowY: 'scroll', // Added to enable scrolling
+    },
+    scrollableContent: {
+        maxHeight: '80vh',
+        overflowY: 'auto',
+        paddingRight: '1em',
     },
     quizHeaderLeft: {
         backgroundColor: colors.green2,
@@ -84,31 +89,109 @@ const useStyles = makeStyles({
 });
 
 const AddNewSet = () => {
-    const { topContainer, rectIcons, rectItem, rectIcon, textArea, fontStyle, quizHeaderLeft, quizHeaderRight, quizContainer, container } = useStyles();
+    const { topContainer, rectIcons, rectItem, rectIcon, textArea, fontStyle, quizHeaderLeft, quizHeaderRight, quizContainer, container, scrollableContent } = useStyles();
+    const [name, setName] = useState('');
+    const [questionList, setQuestionList] = useState([{ question: "", answer: "" }]);
+
+    const addQuestion = () => {
+        setQuestionList([...questionList, { question: "", answer: "" }]);
+    };
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        try {
+            const collectionRef = collection(db, "cards");
+
+            const questions = questionList.map((singleQuestion) => ({
+                question: singleQuestion.question,
+                answer: singleQuestion.answer,
+            }));
+
+            const docData = {
+                name,
+                questions,
+            };
+
+            await addDoc(collectionRef, docData);
+
+            setQuestionList([{ question: "", answer: "" }]);
+            setName('');
+        } catch (error) {
+            console.error('Error adding document: ', error);
+        }
+    };
 
     return (
         <Grid className={container}>
             <Grid container className={topContainer}>
                 <Grid className={textArea}>
-                    <Typography className={fontStyle} style={{fontSize: 48}}>Python for beginners</Typography>
+                    <TextField fullWidth variant="standard" label="Set Card Title" id="fullWidth" onChange={(e) => setName(e.target.value)} />
                 </Grid>
                 <Grid container className={rectIcons}>
                     <Grid item className={rectItem}>
                         <TbEditCircle className={rectIcon} />
                     </Grid>
                     <Grid item className={rectItem}>
-                        <FaPlayCircle className={rectIcon}/>
+                        <FaPlayCircle className={rectIcon} />
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid container className={quizContainer}>
-                <Grid className={quizHeaderLeft}>
-                    <Typography className={fontStyle} style={{fontSize: 32}}>Question</Typography>
-                </Grid>
-                <Grid className={quizHeaderRight}>
-                    <Typography className={fontStyle} style={{fontSize: 32}}>Answer</Typography>
-                </Grid>
-            </Grid>
+            <div className={quizContainer}>
+                <form onSubmit={handleSubmit}>
+                    <div className={scrollableContent}>
+                        {questionList.map((singleQuestion, index) => (
+                            <div key={index}>
+                                <Grid>
+                                    <Grid className={quizHeaderLeft}>
+                                        <Typography className={fontStyle} style={{ fontSize: 32 }}>
+                                            Question
+                                        </Typography>
+                                    </Grid>
+                                    <TextField
+                                        required
+                                        label="Question"
+                                        id="fullWidth"
+                                        variant="outlined"
+                                        value={singleQuestion.question}
+                                        onChange={(e) => {
+                                            const updatedQuestionList = [...questionList];
+                                            updatedQuestionList[index].question = e.target.value;
+                                            setQuestionList(updatedQuestionList);
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid>
+                                    <Grid className={quizHeaderRight}>
+                                        <Typography className={fontStyle} style={{ fontSize: 32 }}>
+                                            Answer
+                                        </Typography>
+                                    </Grid>
+                                    <TextField
+                                        required
+                                        multiline
+                                        label="Answer"
+                                        id="fullWidth"
+                                        variant="outlined"
+                                        value={singleQuestion.answer}
+                                        onChange={(e) => {
+                                            const updatedQuestionList = [...questionList];
+                                            updatedQuestionList[index].answer = e.target.value;
+                                            setQuestionList(updatedQuestionList);
+                                        }}
+                                    />
+                                </Grid>
+                            </div>
+                        ))}
+                    </div>
+                    <Button variant="outlined" color="primary" onClick={addQuestion}>
+                        Add Question
+                    </Button>
+                    <Button variant="outlined" color="secondary" type="submit">
+                        Create
+                    </Button>
+                </form>
+            </div>
         </Grid>
     );
 };
--- frontend/src/components/Home/StudySet.tsx
+++ frontend/src/components/Home/StudySet.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState }  from 'react';
-import { Card, CardContent, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Button, Card, CardContent, Grid, makeStyles, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 // import { ReactComponent as PlusIcon } from '../assets/plus-solid.svg';
 // import PlusIcon from '../../assets/plus-solid.svg';
@@ -13,10 +13,8 @@ const useStyles = makeStyles({
     },
     cardStyle: {
         borderRadius: '25px',
-        padding: '12px',
-        paddingRight: '1%',
-        width: '16em',
-        height: '16em',
+        width: '22em',
+        height: '16em'
     },
     addCard: {
         height: '100%',
@@ -35,10 +33,18 @@ const useStyles = makeStyles({
         margin: 'auto',
         fill: colors.green1,
     },
+    cardHeader: {
+        backgroundColor: 'white',
+        width: '22em',
+        height: '4em',
+        position: 'absolute',
+        borderTopRightRadius: 25,
+        borderTopLeftRadius: 25
+    }
 });
 
 const StudySet = () => {
-  const { gridContainer, cardStyle, addCard, addCardImg } = useStyles();
+  const { gridContainer, cardStyle, addCard, addCardImg, cardHeader } = useStyles();
   const [cards, setCards] = useState<any[]>([]);
   const cardsCollectionRef = collection(db, 'cards')
 
@@ -53,13 +59,12 @@ const StudySet = () => {
     return (
         <>
             <Grid container className={gridContainer} spacing={5}>
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={12} sm={4}>
                     <Link to="/add-new-set" className={addCard}>
                         <Card className={cardStyle}>
                             <CardContent className={addCard}>
                                 <div className={addCardImg}>
-                                {/* <PlusIcon className={addCardImg}/> */}
-                                <img src='plus-solid.svg' alt='Add a New Set'></img>
+                                    <img src='plus-solid.svg' alt='Add a New Set'></img>
                                 </div>
                             </CardContent>
                         </Card>
@@ -67,18 +72,18 @@ const StudySet = () => {
                 </Grid>
                 {
                     cards.map((card) => {
-                        return <Grid item xs={12} sm={3}>
-                                    <Card className={cardStyle}>
+                        return <Grid item xs={12} sm={4}>
+                                    <Card className={cardStyle} style={{backgroundColor: card.color}}>
+                                        <div className={cardHeader}>
+                                            <Typography variant="h5" style={{fontFamily: 'Raleway-Medium', marginTop:'1em'}} align='center'>
+                                                {card.name}
+                                            </Typography>
+                                        </div>
                                         <CardContent>
-                                            <Typography variant="h5" component="h2">
-                                                {card.title}
+                                            <Typography variant="h4" align="center" style={{fontFamily: 'Raleway-Medium', color: 'white', marginTop: '2em'}}>
+                                                {card.questionsSum} Questions
                                             </Typography>
-                                            <Typography color="textSecondary">
-                                                {card.subtitle}
-                                            </Typography>
-                                            <Typography variant="body2" component="p">
-                                                {card.content}
-                                            </Typography>
+                                            <Button variant="text" style={{backgroundColor: 'white', color: card.color, width: '80%', marginLeft: '10%', fontSize: 32, borderRadius: 20, marginTop: '1em'}}>Play</Button>
                                         </CardContent>
                                     </Card>
                                 </Grid>
