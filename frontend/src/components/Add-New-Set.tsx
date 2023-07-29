75ae0134660cd67000012f70208139cb5c2ba698
TushigBili <tushig2003@gmail.com>
2023-07-29 23:48:37 +0800

    connected create quiz front and back end

--- frontend/src/components/Add-New-Set.tsx
+++ frontend/src/components/Add-New-Set.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, CardContent, Grid, makeStyles, Typography, Button } from '@material-ui/core';
 import { colors } from '../colors';
-import { TbEditCircle } from 'react-icons/tb'
-import { FaPlayCircle } from 'react-icons/fa'
 import { doc, addDoc, collection } from "firebase/firestore";
 import { db } from '../firebase'
 import { BsFillPlayFill, BsFillCircleFill } from 'react-icons/bs';
@@ -30,16 +28,16 @@ const useStyles = makeStyles({
         fontSize: '2.5vw',
         display: 'inline',
     },
-    colorCircle:{
+    colorCircle: {
         color: colors.cardBGBlue,
         marginLeft: '2%',
         marginRight: '2%',
         width: '1.5%',
         height: '1.5%',
     },
-    buttonsContainer:{
+    buttonsContainer: {
         position: 'absolute',
-        right: '0', 
+        right: '0',
         width: '30%',
         display: 'flex',
         alignItems: 'center',
@@ -52,7 +50,7 @@ const useStyles = makeStyles({
         fontFamily: 'Raleway-Medium',
         fontSize: 18,
         backgroundColor: colors.gray4,
-        
+
         height: '2.4vw',
         width: '8.2vw',
         marginRight: '5%',
@@ -75,7 +73,7 @@ const useStyles = makeStyles({
         fontSize: 18,
         borderRadius: '20px',
         cursor: 'pointer',
-        
+
         height: '2.4vw',
         width: '8.2vw',
         marginRight: '4%',
@@ -83,23 +81,23 @@ const useStyles = makeStyles({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    editIcon:{
+    editIcon: {
         marginRight: '6px',
     },
 
     //QUESTION AND ANSWER HEADER
-    qaTitleContainer:{
-        marginTop: '3%', 
+    qaTitleContainer: {
+        marginTop: '3%',
         width: '100%',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    qText:{
+    qText: {
         color: colors.gray3,
         fontFamily: 'Raleway-medium',
         fontSize: 14,
-        marginRight: '25%',
+        marginRight: '35%',
     },
     quizContainer: {
         width: '40vw',
@@ -141,12 +139,12 @@ const useStyles = makeStyles({
 
     //STUDY SET Q & A
     qaCardsContainer: {
-        width: '100%',  
-        height:  '100%', 
+        width: '100%',
+        height: '100%',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
     qaPairContainer: {
         marginBottom: '2%',
@@ -155,8 +153,9 @@ const useStyles = makeStyles({
         display: 'flex',
         backgroundColor: colors.gray6,
         borderRadius: '25px',
+        flexDirection: 'row'
     },
-    qaCardStyle:{
+    qaCardStyle: {
         boxShadow: 'none',
         border: 'none',
         width: '50%',
@@ -166,7 +165,6 @@ const useStyles = makeStyles({
         backgroundColor: colors.gray6,
         borderRadius: '25px',
         padding: '2%',
-
         color: colors.gray4,
         fontFamily: 'Raleway-medium',
         fontSize: '17px',
@@ -174,7 +172,7 @@ const useStyles = makeStyles({
         letterSpacing: '-0.5px',
         flexWrap: 'wrap'
     },
-    plusCardStyle:{
+    plusCardStyle: {
         border: '1px solid',
         borderColor: '#E2E3E3',
         backgroundColor: 'white',
@@ -195,7 +193,9 @@ const useStyles = makeStyles({
 })
 
 const AddNewSet = () => {
-    const { topContainer, rectIcons, rectItem, rectIcon, textArea, fontStyle, quizHeaderLeft, quizHeaderRight, quizContainer, container, scrollableContent } = useStyles();
+    const { topContainer, container, colorCircle, studySetTitle, buttonsContainer, qaPairContainer,
+        playButton, playIcon, editButton, editIcon, qaTitleContainer, qText, aText, horizontalLine,
+        qaCardsContainer, plusCardStyle, plusIcon, qaCardStyle } = useStyles();
     const [name, setName] = useState('');
     const [questionList, setQuestionList] = useState([{ question: "", answer: "" }]);
 
@@ -229,136 +229,91 @@ const AddNewSet = () => {
     };
 
     return (
-        <Grid className={container}>
-            <Grid container className={topContainer}>
-                <Grid className={textArea}>
-                    <TextField fullWidth variant="standard" label="Set Card Title" id="fullWidth" onChange={(e) => setName(e.target.value)} />
+        <div className={container}>
+            <div className={topContainer}>
+                <BsFillCircleFill className={colorCircle} />
+                <TextField 
+                    fullWidth 
+                    className={studySetTitle} 
+                    variant="standard" 
+                    placeholder="Set Card Title" 
+                    onChange={(e) => setName(e.target.value)}
+                    InputProps={{
+                        disableUnderline: true,
+                        style: {fontSize: 34}
+                    }} />
+                <div className={buttonsContainer}>
+                    <button className={playButton}>
+                        <BsFillPlayFill className={playIcon} />
+                        Play
+                    </button>
+
+                    <button className={editButton}>
+                        <MdModeEditOutline className={editIcon} />
+                        Edit
+                    </button>
+                </div>
+            </div>
+
+            <Grid container className={qaTitleContainer}>
+                <Grid className={qText}>
+                    <h1>Question</h1>
                 </Grid>
-                <Grid container className={rectIcons}>
-                    <Grid item className={rectItem}>
-                        <TbEditCircle className={rectIcon} />
-                    </Grid>
-                    <Grid item className={rectItem}>
-                        <FaPlayCircle className={rectIcon} />
-                    </Grid>
+                <Grid className={aText}>
+                    <h1>Answer</h1>
                 </Grid>
+                <div className={horizontalLine} />
             </Grid>
-            <div className={quizContainer}>
-                <form onSubmit={handleSubmit}>
-                    <div className={scrollableContent}>
-                        {questionList.map((singleQuestion, index) => (
-                            <div key={index}>
-                                <Grid>
-                                    <Grid className={quizHeaderLeft}>
-                                        <Typography className={fontStyle} style={{ fontSize: 32 }}>
-                                            Question
-                                        </Typography>
-                                    </Grid>
-                                    <TextField
-                                        required
-                                        label="Question"
-                                        id="fullWidth"
-                                        variant="outlined"
-                                        value={singleQuestion.question}
-                                        onChange={(e) => {
-                                            const updatedQuestionList = [...questionList];
-                                            updatedQuestionList[index].question = e.target.value;
-                                            setQuestionList(updatedQuestionList);
-                                        }}
-                                    />
-                                </Grid>
-                                <Grid>
-                                    <Grid className={quizHeaderRight}>
-                                        <Typography className={fontStyle} style={{ fontSize: 32 }}>
-                                            Answer
-                                        </Typography>
-                                    </Grid>
-                                    <TextField
-                                        required
-                                        multiline
-                                        label="Answer"
-                                        id="fullWidth"
-                                        variant="outlined"
-                                        value={singleQuestion.answer}
-                                        onChange={(e) => {
-                                            const updatedQuestionList = [...questionList];
-                                            updatedQuestionList[index].answer = e.target.value;
-                                            setQuestionList(updatedQuestionList);
-                                        }}
-                                    />
-                                </Grid>
-                            </div>
-                        ))}
+
+            <div className={qaCardsContainer}>
+                <form onSubmit={handleSubmit} className={qaCardsContainer}>
+                    {questionList.map((singleQuestion, index) => (
+                        <div key={index} className={qaPairContainer}>
+                            <TextField
+                                required
+                                variant="standard"
+                                fullWidth
+                                className={qaCardStyle}
+                                value={singleQuestion.question}
+                                onChange={(e) => {
+                                    const updatedQuestionList = [...questionList];
+                                    updatedQuestionList[index].question = e.target.value;
+                                    setQuestionList(updatedQuestionList);
+                                }}
+                                InputProps={{
+                                    disableUnderline: true,
+                                    style: {fontSize: 24}
+                                }}
+                            />
+                            <TextField
+                                required
+                                multiline
+                                variant="standard"
+                                fullWidth
+                                className={qaCardStyle}
+                                value={singleQuestion.answer}
+                                onChange={(e) => {
+                                    const updatedQuestionList = [...questionList];
+                                    updatedQuestionList[index].answer = e.target.value;
+                                    setQuestionList(updatedQuestionList);
+                                }}
+                                InputProps={{
+                                    disableUnderline: true,
+                                    style: {fontSize: 24}
+                                }}
+                            />
+                        </div>
+                    ))}
+                    <div className={plusCardStyle} onClick={addQuestion}>
+                        <FiPlus className={plusIcon} />
                     </div>
-                    <Button variant="outlined" color="primary" onClick={addQuestion}>
-                        Add Question
-                    </Button>
                     <Button variant="outlined" color="secondary" type="submit">
                         Create
                     </Button>
                 </form>
             </div>
-        </Grid>
-    // const { container, topContainer, colorCircle, studySetTitle, buttonsContainer, playButton, editButton, playIcon, editIcon, 
-    //     qText, aText, qaTitleContainer, horizontalLine, 
-    //     qaCardsContainer, qaPairContainer, qaCardStyle, plusCardStyle, plusIcon} = useStyles();
-    
-    // //For testing purposes only, add backend later:
-    // const sampleQA: [string, string][] = 
-    //     [['Print statement','print(“hello world”)'], 
-    //     ['Variable assignment','num = 0'],
-    //     ['Integer','A data type for positive or negative whole numbers']];
+        </div>
 
-    // return (
-    //     <div  className={container}>
-
-    //         {/* HEADER */}
-    //         <div className={topContainer}>
-    //             <BsFillCircleFill className={colorCircle} />
-    //             <Typography className={studySetTitle}>Python for beginners</Typography>
-    //             <div className={buttonsContainer}>
-    //                 <button className={playButton}>
-    //                     <BsFillPlayFill className={playIcon}/>
-    //                     Play 
-    //                 </button>
-
-    //                 <button className={editButton}>
-    //                     <MdModeEditOutline className={editIcon}/>
-    //                     Edit 
-    //                 </button>
-    //             </div>
-    //         </div>
-
-    //         {/* QUESTION AND ANSWER HEADER */}
-    //         <Grid container className={qaTitleContainer}>
-    //             <Grid className={qText}>
-    //                 <h1>Question</h1>
-    //             </Grid>
-    //             <Grid className={aText}>
-    //                 <h1>Answer</h1>
-    //             </Grid>
-    //             <div className={horizontalLine} />
-    //         </Grid>
-
-    //         {/* STUDY SET Q&A */}
-    //         <div className={qaCardsContainer}>
-    //         {sampleQA.map((qa) => (
-    //             <div className={qaPairContainer}>
-    //                 <Card className={qaCardStyle}>
-    //                     {qa[0]}
-    //                 </Card>
-    //                 <Card className={qaCardStyle}>
-    //                     {qa[1]}
-    //                 </Card>
-    //             </div>
-    //             ))
-    //         }
-    //         <div className={plusCardStyle}>
-    //             <FiPlus className={plusIcon}/>
-    //         </div>
-    //         </div>
-    //     </div>
-    // );
     )
 };
 
