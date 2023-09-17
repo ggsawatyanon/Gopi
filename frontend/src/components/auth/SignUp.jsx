0ac477a89e5cd4607ae66fe62a8acb45ad18b681
TushigBili <tushig2003@gmail.com>
2023-09-16 22:00:32 -0400

    Created username input, changed some front end, made it so email and username are saved in the collection userinfo in firestore

--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { colors } from '../../colors';
 import { auth, provider, db } from "../../firebase";
 import { TextField } from "@material-ui/core";
-//import { addDoc, collection, doc, setDoc } from "../../firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 
 
 const useStyles = makeStyles(() => ({
@@ -31,18 +31,17 @@ const useStyles = makeStyles(() => ({
         color: colors.black,
         fontSize: '25px',
     },
-    emailBar: {
+    inputBar: {
         fontFamily: 'Raleway-SemiBold, sans-serif',
         fontSize: '0.95em',
         color: colors.gray4,
         backgroundColor: colors.gray5,
         height: '40px',
-        margin: '15px',
+        margin: '10px',
         width: '275px',
         borderColor: colors.gray5,
         borderRadius: '8px',
         border: '1px',
-    
         textIndent: 10
     },
     errorMessage: {
@@ -52,36 +51,6 @@ const useStyles = makeStyles(() => ({
         paddingBottom: '5px',
 
     },
-    passwordBar: {
-        fontFamily: 'Raleway-SemiBold, sans-serif',
-        fontSize: '0.95em',
-        color: colors.gray4,
-        backgroundColor: colors.gray5,
-        height: '40px',
-        margin: '15px',
-        width: '275px',
-        borderColor: colors.gray5,
-        borderRadius: '8px',
-        border: '1px',  
-        marginTop: '8px',
-        textIndent: 10
-    },
-
-    passwordConfBar: {
-        fontFamily: 'Raleway-SemiBold, sans-serif',
-        fontSize: '0.95em',
-        color: colors.gray4,
-        backgroundColor: colors.gray5,
-        height: '40px',
-        margin: '15px',
-        width: '275px',
-        borderColor: colors.gray5,
-        borderRadius: '8px',
-        marginTop: '8px',       
-        border: '1px', 
-        textIndent: 10
-    },
-
 
     SubmitButton: {
         fontFamily: 'Raleway-SemiBold, sans-serif bold',
@@ -94,7 +63,7 @@ const useStyles = makeStyles(() => ({
         borderRadius: '20px',
         textAlign: 'center',
         marginTop: '20px',
-        color: 'white', 
+        color: 'white',
         border: '1px',
     },
     orText: {
@@ -112,7 +81,7 @@ const useStyles = makeStyles(() => ({
         margin: '15px',
         width: '275px',
         textAlign: 'center',
-        color: colors.white, 
+        color: colors.white,
         border: '1px',
 
     },
@@ -142,14 +111,15 @@ const SignUp = () => {
 
     const {
         signUpContainer, headerTitle,
-        emailBar, errorMessage, passwordBar,
+        inputBar, errorMessage,
         SubmitButton, orText, googleButton,
-        otherOption, googleLogo, passwordConfBar,
+        otherOption, googleLogo,
         logoContainer, logo
     } = useStyles();
 
 
     const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConf, setPasswordConf] = useState("");
     const [error, setError] = useState(false);
@@ -175,8 +145,11 @@ const SignUp = () => {
         );
     };
 
+    const validateUsername = (username) => {
+        return /^[A-Za-z0-9]*$/.test(username);
+    }
 
-    const signUp = (e) => {
+    const signUp = async (e) => {
         e.preventDefault();
         if (!validateEmail(email)) {
             setError(true);
@@ -184,15 +157,12 @@ const SignUp = () => {
         if (password.length <= 6) {
             setError(true);
         }
+        if (username.length <= 3 && !validateUsername(username)) {
+            setError(true);
+        }
 
-        createUserWithEmailAndPassword(auth, email, password)
+        createUserWithEmailAndPassword(auth, email, username, password)
             .then((userCredential) => {
-                /*
-                return db.collection('users').doc(userCredential.user.uid).set({
-                    uid: userCredential.user.uid.value,
-                    userName: null
-                });
-                */
                 console.log(userCredential);
                 alert("Account created!")
                 window.location.href = '/';
@@ -202,19 +172,37 @@ const SignUp = () => {
 
             });
 
+        try {
+            const collectionRef = collection(db, "userinfo");
+
+            const docData = {
+                email,
+                username
+            };
+
+            await addDoc(collectionRef, docData);
+
+            setEmail('');
+            setUsername('');
+            setPassword('');
+            setPasswordConf('');
+        } catch (error) {
+            console.error('Error adding document: ', error);
+        }
+
     };
 
     return (
         <div>
             <div className={logoContainer}>
-                <div style={{fontFamily: 'Raleway', fontSize: 36, fontWeight: 500, paddingLeft: '4em', color: 'white'}}>Welcome to</div>
+                <div style={{ fontFamily: 'Raleway', fontSize: 36, fontWeight: 500, paddingLeft: '4em', color: 'white' }}>Welcome to</div>
                 <img className={logo} src="gopi-white-cropped.png" alt="Gopi Logo" />
             </div>
             <div className={signUpContainer}>
                 <h1 className={headerTitle}>Sign Up</h1>
                 <form onSubmit={signUp}>
-                    <input required 
-                        className={emailBar}
+                    <input required
+                        className={inputBar}
                         type={email}
                         placeholder="Email"
                         value={email}
@@ -222,11 +210,22 @@ const SignUp = () => {
                     />
                     {error && !validateEmail(email) ?
                         <label className={errorMessage}> Please enter a valid email.</label> : ""}
+                    <br></br>
+
+                    <input required
+                        className={inputBar}
+                        type={username}
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    {error && !validateUsername(username) ?
+                        <label className={errorMessage}> Please enter a username with only letters and numbers.</label> : ""}
 
                     <br></br>
                     <input required
-                        className={passwordBar}
-                        type={password}
+                        className={inputBar}
+                        type="password"
                         placeholder="Create a Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
@@ -236,9 +235,9 @@ const SignUp = () => {
                         <label className={errorMessage}> Password must be at least 6 characters.
                         </label> : ""}
 
-                    <input required 
-                        className={passwordConfBar}
-                        type={password}
+                    <input required
+                        className={inputBar}
+                        type="password"
                         placeholder="Confirm your password"
                         value={passwordConf}
                         onChange={(e) => setPasswordConf(e.target.value)}
